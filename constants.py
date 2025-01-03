622c7ad0696d6eefe2e1ac39b97eb63daee03c5e
Ng Lok Chun <nglokchun@gmail.com>
2025-01-03 14:44:04 +0800

    14:44

---
+++ constants.py
@@ -0,0 +1,2 @@
+distance_value = "marathon"
+number_of_weeks_to_train = 16
---
+++ src/constants.py
@@ -0,0 +1,28 @@
+distance_value = "marathon"
+number_of_weeks_to_train = 16
+
+speed_run_sessions = [
+    "100m Intervals",
+    "400m Intervals",
+    "800m Intervals",
+    "Hill Repeats",
+]
+
+medium_run_sessions = [
+    "Fartlek",
+    "Tempo",
+    "Progression",
+    "Long Intervals",
+    "Threshold",
+]
+
+long_run_sessions = [
+    "Long Run",
+    "Long Run",
+    "Long Run",
+    "Long Run (Sprint Finish)",
+    "Long Run (Sprint Finish)",
+    "Long Run (Progression)",
+    "Long Run (Progression)",
+    "Long Run (Race Pace)",
+]
